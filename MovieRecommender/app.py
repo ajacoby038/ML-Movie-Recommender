5a075c90236caa636a9515bebb2d9c24ee9a5f10
Alek Jacoby <alekjacoby@gmail.com>
2022-08-16 19:42:38 -0400

    fix display

--- MovieRecommender/app.py
+++ MovieRecommender/app.py
@@ -3,3 +3,7 @@ import pandas as pd
 movies = pd.read_csv('resources/movies.csv')
 ratings = pd.read_csv('resources/ratings.csv')
 print("This dataset has ", len(movies), " movies and ", len(ratings), " ratings")
+
+dataset_complete = ratings.pivot(index = 'movieId', columns = 'userId', values = 'rating')
+dataset_complete.fillna(0, inplace=True)
+print(dataset_complete.head())
